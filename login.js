0eaf119daee324b07742fdcc72ef960a04081624
SABARITHAN-P <sabarithanpalanivel@gmail.com>
2025-05-22 23:05:01 +0530

    creation of login page

---
+++ login.js
@@ -0,0 +1,9 @@
+function SignupPage() {
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("signup-form").style.display = "block";
+}
+
+function LoginPage() {
+  document.getElementById("signup-form").style.display = "none";
+  document.getElementById("login-form").style.display = "block";
+}
