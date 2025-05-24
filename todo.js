292d581ebcff16cdf88711fddd52ac5a4ce9bdca
SABARITHAN-P <sabarithanpalanivel@gmail.com>
2025-05-24 22:49:48 +0530

    addition of new page

---
+++ todo.js
@@ -0,0 +1,21 @@
+var box1 = document.getElementById("add");
+console.log(box1.value)
+
+function TodoPage(){
+    window.location.href = "todo.html";
+}
+function all_task(){
+
+}
+function active_task(){
+
+}
+function completed_task(){
+ 
+}
+function edit_task(){
+    
+}
+function delete_task(){
+
+}
